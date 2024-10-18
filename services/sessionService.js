bdf0a528bb611dd1b05092bacbe989a1602a573e
Balogun2705 <balogunsemiat2019@gmail.com>
2024-10-18 05:07:04 +0100

    set-expiration-time

--- services/sessionService.js
+++ services/sessionService.js
@@ -3,7 +3,7 @@ const { JWT_SECRET } = require("../constants/index")
 
 const createToken = (email) => {
     try{
-        const token = jwt.sign({email}, JWT_SECRET, {expiresIn: "10m"})
+        const token = jwt.sign({email}, JWT_SECRET, {expiresIn: "1h"})
         
         return token
     } catch(error) {
